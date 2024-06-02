a9eef625e18bae7f6be557d79c9ebceb14c9a800
Govindapalle Vijaychandra <118375565+Vijaychandra-Govindapalle@users.noreply.github.com>
2024-06-02 13:01:34 +0530

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -5,6 +5,7 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
  const server = net.createServer((socket) => {
+   socket.write('HTTP/1.1 200 OK\r\n\r\n')
    socket.on("close", () => {
      socket.end();
      server.close();
@@ -12,3 +13,4 @@ console.log("Logs from your program will appear here!");
  });
 
  server.listen(4221, "localhost");
+
