27ec71989c35a9c4813353c8f07c6c661b72fdbe
Govindapalle Vijaychandra <118375565+Vijaychandra-Govindapalle@users.noreply.github.com>
2024-06-02 16:22:16 +0530

    pass 4th stage

--- app/main.js
+++ app/main.js
@@ -7,7 +7,7 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
  const server = net.createServer((socket) => {
     socket.on("data",(data) =>{
-       let path = data.toString().split(" ")[1];
+       const path = data.toString().split(" ")[1];
      //  const responseStatus = path === "/" ? "200 OK" : "404 Not Found";
      //  socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`);
        if (path.startsWith('/echo')){
