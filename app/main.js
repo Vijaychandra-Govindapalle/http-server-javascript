139147be136a342afdeee6caaadf604e50a22403
Govindapalle Vijaychandra <118375565+Vijaychandra-Govindapalle@users.noreply.github.com>
2024-06-02 20:10:22 +0530

    Pass 7th stage

--- app/main.js
+++ app/main.js
@@ -24,14 +24,14 @@ console.log("Logs from your program will appear here!");
             }
             else{ 
                 socket.write("HTTP/1.1 404 Not Found\r\n\r\n"); 
-                
             }
+            socket.end()
            
        }
        else {
-      // const responseStatus = path === "/" ? "200 OK" : "404 Not Found";
-       //socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`);
-       //socket.end()
+       const responseStatus = path === "/" ? "200 OK" : "404 Not Found";
+       socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`);
+       socket.end()
        }
        socket.end()
       
