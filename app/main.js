8990ce448d4c15a88a699d7ffb12deb92f1e5183
Govindapalle Vijaychandra <118375565+Vijaychandra-Govindapalle@users.noreply.github.com>
2024-06-02 16:03:20 +0530

    pass 4th stage

--- app/main.js
+++ app/main.js
@@ -11,7 +11,7 @@ console.log("Logs from your program will appear here!");
      //  const responseStatus = path === "/" ? "200 OK" : "404 Not Found";
      //  socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`);
        if (path.includes('/echo')){
-          const responeBody = path[1].split("/echo/")[1]
+          const responeBody = path.split("/echo/")[1]
           socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${responeBody.length()}\r\n\r\n${responeBody}`)
        }
 
