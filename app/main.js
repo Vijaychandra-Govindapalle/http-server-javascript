48de2793c5bb404083e56321b7054bc234036ae6
Govindapalle Vijaychandra <118375565+Vijaychandra-Govindapalle@users.noreply.github.com>
2024-06-03 17:00:43 +0530

    Pass 11th stage

--- app/main.js
+++ app/main.js
@@ -15,7 +15,7 @@ console.log("Logs from your program will appear here!");
            const compressionScheme = request.split("\r\n")[2].split('Accept-Encoding: ')[1];
            if(compressionScheme.includes("gzip" ) ){
             const randomString = path.split('/echo/')[1]
-            const  gzipEncodedData = zlib.gzipSync(randomString).toString('hex');
+            const  gzipEncodedData = zlib.gzipSync(randomString).toString('hex').toString('hex');
             /*const  gzipEncodedData = zlib.gzip(randomString,(error,buffer)=>{
                 if(!error){
                     console.log(buffer.toString('hex'));
